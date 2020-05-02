d840bc3faf01d2bcfe84e693ea9433e5272da316
Johannes Boghaert <johannes@dhcp-213-144-153-95.init7.net>
2020-05-02 17:09:19 +0200

    restructured endpoints

--- packages/configuration_manager/server.py
+++ packages/configuration_manager/server.py
@@ -55,40 +55,44 @@ def clear_logs():
 
 #FLEET CONFIGURATION
 #Default response
-@app.route("/fleet/")
-def home():
-    return json.dumps(fleet_manager.default_response())
+@app.route("/fleet/<fleet>")
+def fleet_home(fleet):
+    return json.dumps(fleet_manager.default_response(fleet))
 
 """
 #Passive messaging
-@app.route("/fleet/configuration/info/<config_name>", methods=['GET'])
-def get_config(config_name):
-    return json.dumps(fleet_manager.configuration_info(config_name))
-
+@app.route("/fleet/configuration/list/<fleet>")
+def fleet_configs(fleet):
+    return json.dumps(fleet_manager.configuration_list(fleet))
+"""
+@app.route("/fleet/configuration/info/<config_name>/<fleet>", methods=['GET'])
+def fleet_get_config(config_name, fleet):
+    return json.dumps(fleet_manager.configuration_info(config_name, fleet))
+"""
 #Active messaging
-@app.route("/fleet/configuration/set/<config_name>", methods=['GET'])
-def load_config(config_name):
-    return json.dumps(fleet_manager.configuration_set_config(config_name))
-@app.route("/fleet/pull/<image_name>")
-def pull_image(image_name):
-    return json.dumps(fleet_manager.pull_image(image_name))
+@app.route("/fleet/configuration/set/<config_name>/<fleet>", methods=['GET'])
+def fleet_load_config(config_name, fleet):
+    return json.dumps(fleet_manager.configuration_set_config(config_name, fleet))
+@app.route("/fleet/pull/<image_name>/<fleet>")
+def fleet_pull_image(image_name, fleet):
+    return json.dumps(fleet_manager.pull_image(image_name, fleet))
 @app.route("/fleet/monitor/<id>", methods=['GET'])
-def get_job_status(id):
+def fleet_get_job_status(id):
     return json.dumps(fleet_manager.monitor_id(id))
-@app.route("/fleet/list")
-def get_device_list():
+@app.route("/fleet/list/<fleet>")
+def fleet_get_device_list():
     return json.dumps(fleet_manager.list())
 @app.route("fleet/info/<fleet>")
-def get_fleet_info(fleet):
+def fleet_get_fleet_info(fleet):
     return json.dumps(fleet_manager.info(fleet))
-@app.route("fleet/image/status")
-def get_image_status():
+@app.route("fleet/image/status/<fleet>")
+def fleet_get_image_status():
     return json.dumps(fleet_manager.image_status())
-@app.route("fleet/scan")
-def scan_for_devices():
+@app.route("fleet/scan/<fleet>")
+def fleet_scan_for_devices():
     return json.dumps(fleet_manager.scan())
-@app.route("/fleet/clearlogs")
-def clear_logs():
+@app.route("/fleet/clearlogs/<fleet>")
+def fleet_clear_logs():
     return json.dumps(fleet_manager.clear_job_log())
 """
 
